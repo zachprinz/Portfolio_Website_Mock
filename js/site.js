e811cfa291f1f8f39e04a0104de5bc07bbaaf86d
zachprinz <zprinzbach@gmail.com>
2014-05-03 01:28:20 -0400

    asdf

--- js/site.js
+++ js/site.js
@@ -7,8 +7,6 @@ Site = function(width,height){
 	this.currentSeries = 0;
 	this.imageWidth = this.width/3.3;
 	this.imageHeight = this.height/1.8;
-	this.shiftRight();
-	this.draw();
 };
 
 Site.prototype = {
